11c458deadfb61569c896aeb6b1fb2f44747b746
ltram16 <ltram16@gmail.com>
2023-04-03 12:51:19 -0700

    added crate post handlebars

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -17,6 +17,24 @@ router.get("/", withAuth, async (req, res) => {
   // refer to admin-all-posts.handlebars write the code to display the posts
 });
 
+router.get("/create", withAuth, async (req, res) => {
+res.render("admin-create-post", { layout: "dashboard" });
+});
+
+router.post("/create", withAuth, async (req, res) => {
+  try {
+  await Post.create({
+    title: req.body.title,
+    body: req.body.body,
+    userId: req.session.userId,
+  })
+  
+  res.redirect('/dashboard');
+} catch (err) {
+  res.status(500).json(err);
+}
+})
+
 // TODO - create logic for the GET route for /new that renders the new post page
 // It should display a form for creating a new post
 
