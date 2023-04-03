d79aaa9d40fdf2c851bb0bef4c9ec43550fd16c9
ltram16 <ltram16@gmail.com>
2023-04-03 13:40:01 -0700

    edit dashboard and create post handlebars

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -1,19 +1,24 @@
 
 // Dashboard Routes
-// This is a set of routes that will be used to render the dashboard pages.
-// All of these routes will be protected by the withAuth middleware function.
-
 const router = require("express").Router();
-const { Post } = require("../models/");
+const { Post, User, Comment } = require("../models/");
 const withAuth = require("../utils/auth");
 
 // TODO - create logic for the GET route for / that renders the dashboard homepage
 // It should display all of the posts created by the logged in user
 router.get("/", withAuth, async (req, res) => {
-  // TODO - retrieve all posts from the database for the logged in user
-  // render the dashboard template with the posts retrieved from the database
-  //default layout is set to main.handlebars, layout need to be changed to dashboard to use dashboard.handlebars
-  res.render("admin-all-posts", { layout: "dashboard" });
+  const postsData = await Post.findAll({ 
+    where: { userId: req.session.userId }, 
+    include: [
+      {
+        model: User,
+        attributes: ["username"],
+      }
+    ],
+  });
+  const posts = postsData.map((post) => post.get({ plain: true }));
+  console.log(posts);
+  res.render("admin-all-posts", { layout: "dashboard", posts });
   // refer to admin-all-posts.handlebars write the code to display the posts
 });
 
